82b9f04c10d1d352044782bf22a6e4779a099ef9
MasenkoSS <83786081+MasenkoSS@users.noreply.github.com>
2021-07-17 17:14:52 +0000

    Deploying to gh-pages from @ Sergey-Kapitanchuk/team-progekt@cab727b8857c9ffd85cf3295fc4db6b9f850184f 🚀

--- src.d84cca52.js
+++ src.d84cca52.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-progekt/src.7d79a82b.js.map
+//# sourceMappingURL=/team-progekt/src.d84cca52.js.map
