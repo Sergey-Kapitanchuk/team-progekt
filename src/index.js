ebf660f52c0ee3180ce78701c213c4b02a31a236
Andrey1Mishchenko <andrej1mishchenko@gmail.com>
2021-07-22 00:00:13 +0300

    добавлено моб меню и скрипт

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './sass/main.scss';
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const opacityMenuBtn = document.querySelector('.js-opacity');
+  const opacityMenuBtnA = document.querySelector('.js-opacityA');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    opacityMenuBtn.classList.toggle('is-close');
+    opacityMenuBtnA.classList.toggle('is-close');
+
+    const scrollLockMethod = !isMenuOpenjs
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    opacityMenuBtn.classList.remove('is-close');
+    opacityMenuBtnA.classList.remove('is-close');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
